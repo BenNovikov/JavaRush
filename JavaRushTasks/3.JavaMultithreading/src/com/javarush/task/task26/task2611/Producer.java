6208544c3587ddb43345608431dfd73a1fa0d2c0
BenNovikov <novikov@inet.ua>
2017-09-16 01:27:01 +0300

    task2612 started

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task26/task2611/Producer.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task26.task2611;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Ben on 2017-09-16.
+ */
+public class Producer implements Runnable {
+    private ConcurrentHashMap<String, String> map;
+
+    public Producer(ConcurrentHashMap map) {
+        this.map = map;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int i = 1;
+            while (true) {
+                map.put(Integer.toString(i), "Some text for " + i);
+                Thread.sleep(500);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(Thread.currentThread().getName() + " thread was terminated");
+        }
+    }
+}
