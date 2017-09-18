5125245341f858588b42101251b58e5534cd59a8
BenNovikov <novikov@inet.ua>
2017-09-18 23:46:16 +0300

    task3008 Chat started

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task30.task3008;
+
+/**
+ * Created by Ben on 2017-09-18.
+ */
+public enum MessageType {
+}
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task30.task3008;
+
+/**
+ * Created by Ben on 2017-09-18.
+ */
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
