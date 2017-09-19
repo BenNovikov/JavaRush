b33f74e670e63748bc56e8a2921b7d23ad0b99f9
BenNovikov <novikov@inet.ua>
2017-09-19 17:58:20 +0300

    task3008 beating validator at part 8

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,36 +3,69 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Ben on 2017-09-18.
  */
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
-        int port = ConsoleHelper.readInt();
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(port);
-            System.out.println("Server started");
+        ConsoleHelper.writeMessage("Input server port number: ");
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("Server started");
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-        finally {
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Map.Entry<String, Connection> connection : connectionMap.entrySet()) {
             try {
-                serverSocket.close();
+                connection.getValue().send(message);
             }
-            catch (IOException e2) {
-                e2.printStackTrace();
+            catch (IOException e) {
+                System.out.println("Sending message Error!");
             }
         }
     }
 
-    private static class Handler {
+    private static class Handler extends Thread {
         private Socket socket;
 
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            Message message = new Message(MessageType.NAME_REQUEST);
+            connection.send(message);
+            message = connection.receive();
+            if (message.getType() == MessageType.USER_NAME) {
+                String name = message.getData();
+                if (!connectionMap.containsKey(name)) {
+                    connectionMap.putIfAbsent(name, connection);
+                    connection.send(new Message(MessageType.NAME_ACCEPTED));
+                    return name;
+                }
+            }
+
+            System.out.println("Server handshake failed! Resetting connection. ");
+            serverHandshake(connection);
+
+            return null;
         }
     }
 }
