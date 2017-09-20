19c35ad501155f0426050fa62bfcbbcc16d0530e
BenNovikov <novikov@inet.ua>
2017-09-20 22:02:25 +0300

    task3008 part20 started

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,8 +1,13 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Created by Ben on 2017-09-20.
@@ -40,26 +45,42 @@ public class BotClient extends Client {
         @Override
         protected void processIncomingMessage(String message) {
             ConsoleHelper.writeMessage(message);
-            String[] messageData = message.split(": ");
-            switch (messageData[1]) {
-                case "дата" :
-                    break;
-                case "день" :
-                    break;
-                case "месяц" :
-                    break;
-                case "год" :
-                    break;
-                case "время" :
-                    break;
-                case "час" :
-                    break;
-                case "минуты" :
-                    break;
-                case "секунды" :
-                    break;
-                default:
+            if (message != null && message.contains(": ")) {
+                String[] messageData = message.split(": ");
 
+                Calendar cal = Calendar.getInstance();
+                SimpleDateFormat dateFormat = new SimpleDateFormat();
+                switch (messageData[1]) {
+                    case "дата":
+                        dateFormat = new SimpleDateFormat("d.MM.YYYY");
+                        break;
+                    case "день":
+                        dateFormat = new SimpleDateFormat("d");
+                        break;
+                    case "месяц":
+                        dateFormat = new SimpleDateFormat("MMMM");
+                        break;
+                    case "год":
+                        dateFormat = new SimpleDateFormat("YYYY");
+                        break;
+                    case "время":
+                        dateFormat = new SimpleDateFormat("H:mm:ss");
+                        break;
+                    case "час":
+                        dateFormat = new SimpleDateFormat("H");
+                        break;
+                    case "минуты":
+                        dateFormat = new SimpleDateFormat("m");
+                        break;
+                    case "секунды":
+                        dateFormat = new SimpleDateFormat("s");
+                        break;
+                    default:
+                        return;
+                }
+                
+                String messageToSend = "Информация для " + messageData[0] + ": " + dateFormat.format(cal.getTime());
+                sendTextMessage(messageToSend);
             }
         }
     }
