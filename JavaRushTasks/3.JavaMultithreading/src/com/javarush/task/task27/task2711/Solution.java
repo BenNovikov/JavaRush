29a54b527cba684734b4deb0c3f395567b22e80a
BenNovikov <novikov@inet.ua>
2017-09-25 01:47:24 +0300

    task2710 started

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task27.task2711;
+
+import java.util.concurrent.CountDownLatch;
+
+/* 
+CountDownLatch
+1. Из класса Solution должно быть удалено поле lock.
+2. Из метода someMethod должен быть удален synchronized блок.
+3. В методе someMethod должен быть вызван метод await без параметров у объекта сохраненного в поле latch.
+4. В методе someMethod должен быть вызван метод retrieveValue.
+5. В методе someMethod должен быть вызван метод countDown у объекта сохраненного в поле latch.
+*/
+public class Solution {
+    private volatile boolean isWaitingForValue = true;
+    CountDownLatch latch = new CountDownLatch(1);
+
+    public void someMethod() throws InterruptedException {
+        latch.await();
+        retrieveValue();
+        latch.countDown();
+    }
+
+    void retrieveValue() {
+        System.out.println("Value retrieved.");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
