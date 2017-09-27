ad0155ca49cf69ad9f044148ee902d7e7478ce99
BenNovikov <novikov@inet.ua>
2017-09-27 14:57:10 +0300

    task3110 part11 started

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task31.task3110.command;
 
 import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
 
 public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception {
+
+        return null;
+    }
 }
