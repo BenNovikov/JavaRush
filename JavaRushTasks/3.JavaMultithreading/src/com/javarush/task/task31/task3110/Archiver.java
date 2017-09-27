63af16df516d5d2a2a14390af6628fb583466bce
BenNovikov <novikov@inet.ua>
2017-09-27 04:19:34 +0300

    task3110 part8 complete

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,11 +1,11 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /*
@@ -18,27 +18,50 @@ public class Archiver {
     private static final String helperTarget = "D:/Workshop/Java/Javarush/source.zip";
 
     public static void main(String[] args) {
-        String archivePathAndName = "", sourcePathAndName = "";
+//        String archivePathAndName = "", sourcePathAndName = "";
+//
+//        try (BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)))) {
+//            System.out.print("Enter full path and file name of the zip archive: ");
+//            archivePathAndName = reader.readLine();
+//            if (archivePathAndName.equals("")) archivePathAndName = helperTarget;
+//            ZipFileManager manager = new ZipFileManager(Paths.get(archivePathAndName));
+//
+//            System.out.print("Enter full path and file name of the source file: ");
+//            sourcePathAndName = reader.readLine();
+//            if (sourcePathAndName.equals("")) sourcePathAndName = helperSource;
+//            try {
+//                manager.createZip(Paths.get(sourcePathAndName));
+//            }
+//            catch (Exception e) {
+//            }
+//        }
+//        catch (IOException e) {
+//        }
 
-        try (BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)))) {
-            System.out.print("Enter full path and file name of the zip archive: ");
-            archivePathAndName = reader.readLine();
-            if (archivePathAndName.equals("")) archivePathAndName = helperTarget;
-            ZipFileManager manager = new ZipFileManager(Paths.get(archivePathAndName));
-
-            System.out.print("Enter full path and file name of the source file: ");
-            sourcePathAndName = reader.readLine();
-            if (sourcePathAndName.equals("")) sourcePathAndName = helperSource;
+        Operation operation = null;
+        do {
             try {
-                manager.createZip(Paths.get(sourcePathAndName));
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            }
+            catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
             }
             catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
-        }
-        catch (IOException e) {
-        }
+        } while (operation != Operation.EXIT);
+    }
 
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ExitCommand.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ExitCommand.java
@@ -4,7 +4,7 @@ import com.javarush.task.task31.task3110.ConsoleHelper;
 
 public class ExitCommand implements Command {
     @Override
-    public void execute() {
+    public void execute() throws Exception {
         ConsoleHelper.writeMessage("До встречи!");
     }
 }
