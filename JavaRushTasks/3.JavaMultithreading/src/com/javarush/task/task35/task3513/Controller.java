010ef8930627d7e4b543c3488727058e51494e27
BenNovikov <novikov@inet.ua>
2017-10-06 02:39:13 +0300

    task3513 part3

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by Ben on 2017-10-06.
+ */
+public class Controller {
+}
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by Ben on 2017-10-06.
+ */
+public class Main {
+    public static void main (String[] args) {
+        
+    }
+}
